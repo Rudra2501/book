e7b3f532ca4e4eecfdbbb8f384fc3b853b20a9d5
Rudra2501 <rpr250102@gmail.com>
2023-03-12 22:30:27 +0530

    DOM LOADED

--- script.js
+++ script.js
@@ -1,20 +1,67 @@
-document.getElementById("myButton").addEventListener("click", crudAPI);
+document.addEventListener("DOMContentLoaded", pageLoaded);
+document.getElementById("myButton").addEventListener("click", addData);
 
-function crudAPI() {
+function pageLoaded() {
+  axios
+    .get(
+      " https://crudcrud.com/api/5788d52981594ecf8454d99464004282/MyUserData"
+    )
+    .then((obj) => {
+      let data = obj.data;
+      data.forEach((item) => displayUserData(item));
+    })
+    .catch((err) => console.log(err));
+}
+
+function addData() {
   let name = document.getElementById("userName").value;
-  let email = document.getElementById("mail").value;
+  let mail = document.getElementById("mail").value;
   let number = document.getElementById("number").value;
 
-  const myDetails = {
+  const userData = {
     name,
-    email,
+    mail,
     number,
   };
 
   axios
     .post(
-      "https://crudcrud.com/api/019b55a81f3c438da38bd2dc0ec9dc7c/Pratap/",
-      myDetails
+      "https://crudcrud.com/api/5788d52981594ecf8454d99464004282/MyUserData",
+      userData
     )
+    .then((result) => console.log(result))
     .catch((err) => console.log(err));
+
+  displayUserData(userData);
+}
+
+function displayUserData(myObj) {
+  let node = document.createElement("li"); // Creating new li item
+  let text = document.createTextNode(
+    `${myObj["name"]} - ${myObj["mail"]} - ${myObj["number"]}`
+  ); // Creating text node for storing the text
+
+  let btnEdit = document.createElement("input"); // Creating Edit button
+  btnEdit.type = "button";
+  btnEdit.value = "Edit";
+
+  let btnDelete = document.createElement("input"); // Creating Delete button
+  btnDelete.type = "button";
+  btnDelete.value = "Delete";
+  btnDelete.onclick = () => {
+    listItems.removeChild(node);
+    axios
+      .delete(
+        `https://crudcrud.com/api/5788d52981594ecf8454d99464004282/MyUserData/${myObj["_id"]}`
+      )
+      .catch((err) => console.log(err));
+  };
+
+  let listItems = document.getElementById("listItems"); // Storing the unordered list element inside a variable
+
+  node.appendChild(text); // Appending the text inside the list item
+  node.appendChild(btnEdit); // Appending the edit button insdie the list item
+  node.appendChild(btnDelete); // Appending the delete button insdie the list item
+
+  listItems.appendChild(node); // Appending the li item inside the unordered list
 }
