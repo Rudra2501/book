c661eb2aa13b93f25aa333592b499901f3e1604b
Rudra2501 <rpr250102@gmail.com>
2023-03-14 13:00:35 +0530

    edit add

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", pageLoaded);
 document.getElementById("myButton").addEventListener("click", addData);
-
 function pageLoaded() {
   axios
     .get(
@@ -12,18 +11,15 @@ function pageLoaded() {
     })
     .catch((err) => console.log(err));
 }
-
 function addData() {
   let name = document.getElementById("userName").value;
   let mail = document.getElementById("mail").value;
   let number = document.getElementById("number").value;
-
   const userData = {
     name,
     mail,
     number,
   };
-
   axios
     .post(
       "https://crudcrud.com/api/5788d52981594ecf8454d99464004282/MyUserData",
@@ -31,15 +27,25 @@ function addData() {
     )
     .then((result) => console.log(result))
     .catch((err) => console.log(err));
-
   displayUserData(userData);
 }
-
 function displayUserData(myObj) {
   let node = document.createElement("li"); // Creating new li item
   let text = document.createTextNode(
     `${myObj["name"]} - ${myObj["mail"]} - ${myObj["number"]}`
   ); // Creating text node for storing the text
+
+  let btnEdit = document.createElement("input"); // Creating Edit button
+  btnEdit.type = "button";
+  btnEdit.value = "Edit";
+  btnEdit.onclick = () => {
+    listItems.removeChild(node);
+    axios
+      .edit(
+        `https://crudcrud.com/api/5788d52981594ecf8454d99464004282/MyUserData/${myObj["_id"]}`
+      )
+      .catch((err) => console.log(err));
+  };
 
   let btnDelete = document.createElement("input"); // Creating Delete button
   btnDelete.type = "button";
@@ -52,11 +58,11 @@ function displayUserData(myObj) {
       )
       .catch((err) => console.log(err));
   };
-
   let listItems = document.getElementById("listItems"); // Storing the unordered list element inside a variable
 
   node.appendChild(text); // Appending the text inside the list item
-  node.appendChild(btnDelete); // Appending the delete button insdie the list item
+  node.appendChild(btnEdit); // Appending the edit button inside the list item
+  node.appendChild(btnDelete); // Appending the delete button inside the list item
 
   listItems.appendChild(node); // Appending the li item inside the unordered list
 }
